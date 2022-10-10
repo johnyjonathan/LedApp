60e5fdb23610233d503a9f9a19bb8c7db9b352c1
johnyjonathan <michaltaszycki@hotmail.com>
2022-10-10 21:18:06 +0200

    led count - bug fix

--- LedApp/utilities.py
+++ LedApp/utilities.py
@@ -7,10 +7,11 @@ from LedApp.models import settings
 
 pixel_pin = board.D18
 ORDER = neopixel.GRB
-num_from_db = settings.objects.get(name="led_strip")
-num_of_pixels = int(num_from_db.led_num)
+
 
 def setColor(rgb,brightness):
+    num_from_db = settings.objects.get(name="led_strip")
+    num_of_pixels = int(num_from_db.led_num)
 
     pixels = neopixel.NeoPixel(
         pixel_pin,num_of_pixels, brightness = float(brightness), auto_write = False, pixel_order = ORDER
